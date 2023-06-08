a4f8af12ed79ac2e519c8f93ecd751c4cc427a6d
MaissWanous <maisswanous@gmail.com>
2023-06-08 02:56:12 -0700

    Check Email

--- employee/js/addTeacher.js
+++ employee/js/addTeacher.js
@@ -10,7 +10,10 @@ module.exports = {
     var Lname;
     var subject;
     var wage;
+    var Email;
     var obj1;
+    var Cemail = "false";
+    let accept_email = ["hotmail", "gmail"];
 
     app.post("/teachInfo", (req, res) => {
       Fname = req.body.f_name;
@@ -18,17 +21,24 @@ module.exports = {
       Lname = req.body.l_name;
       subject = req.body.subject;
       wage = req.body.wage;
+      Email = req.body, Email;
 
-      obj1 = {
-        Fname: Fname,
-        Mname: Mname,
-        Lname: Lname,
-        subject: subject,
-        wage: wage,
-      };
+      obj1 = { Fname: Fname, Mname: Mname, Lname: Lname, subject: subject, wage: wage };
       res.send();
     });
     app.post("/confirm", function (req, res) {
+
+      let emaill = email.toLowerCase();
+      if (email != "") {
+        for (let i = 0; i < accept_email.length; i++)
+          if (emaill.search("@" + accept_email[i] + ".com") != -1) {
+            Cemail = true;
+            break;
+          }
+      }
+      if (Cemail == false && email != "") {
+        test1 = "Not accept email";
+      }
       MongoClient.connect(url, async function (err, db) {
         if (err) throw err;
         var dbo = db.db("languages");
@@ -38,13 +48,14 @@ module.exports = {
           console.log("add new teacher ");
         });
       });
-      res.send("your request has been cinfirmed");
+      res.send("your request has been confirmed");
     });
     app.get("/link1", function (req, res) {
       let object = {
         Fname: Fname,
         Mname: Mname,
         Lname: Lname,
+        Email: Email,
         subject: subject,
         wage: wage,
       };
