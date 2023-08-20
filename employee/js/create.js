dac5943ca4b7436f0a42479d8ed78b650eb837e5
MaissWanous <maisswanous@gmail.com>
2023-08-20 02:45:33 -0700

    choose teacher create.js

--- employee/js/create.js
+++ employee/js/create.js
@@ -8,44 +8,11 @@ module.exports = {
     var subjCnt = [];
     var teacherName = [];
     var selected_subject = ["German", "Frensh", "Russian", "Kids"];
-    var selected_level = [
-      "Beginner",
-      "Intermediate",
-      "Advanced",
-      "ibt1",
-      "ibt2",
-      "pbt1",
-      "pbt2",
-    ];
+    var selected_level = ["Beginner", "Intermediate", "Advanced", "ibt1", "ibt2", "pbt1", "pbt2",];
     var cnt;
     app.get("/creat", function (req, res) {
       res.sendFile(dic + "/html/create.html");
-      for (let i = 0; i < selected_subject.length; i++) {
-        cnt = collection
-          .count({ selected_subject: selected_subject[i] })
-          .then((count) => {
-            // console.log(`Number of documents with selected_subject : ${count}`);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-        subjCnt.push([cnt, selected_subject[i]]);
-        // console.log(subjCnt.length);
-      }
-      for (let i = 0; i < selected_level.length; i++) {
-        cnt = collection
-          .count({
-            selected_subject: "English",
-            selected_level: selected_level[i],
-          })
-          .then((count) => {
-            //console.log(`Number of documents with selected_subject : ${count}`);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-        subjCnt.push([cnt, selected_level[i]]);
-      }
+
     });
     //
     var number_student;
@@ -59,40 +26,38 @@ module.exports = {
     var subject;
     var rowIndex;
 
-    app.post("/course", function (req, res) {
+    app.post("/course", async function (req, res) {
       subject = req.body.course;
-      subjCnt.forEach(function (row, rowIndex) {
-        row.forEach(function (element, columnIndex) {
-          if (element === subject) {
-            console.log(
-              "Element found at row " + rowIndex + " and column " + columnIndex
-            );
-            found = true;
-          }
-        });
-      });
       //choose teacher
-      collteacher.find({ subject: subject }, function (err, docs) {
-        console.log(typeof docs);
-        if (err) {
-          console.log(err);
-
-          return;
-        }
-        teacherName = data.toArray();
-        console.log(teacherName);
-        doc = docs;
-        const teachers = [];
-        for (let i = 0; i < teacherName.length; i++) {
-          teachers.push({
-            value: teacherName[i].email,
-            text: teacherName[i].email,
-          });
-        }
-        app.get("/teachers", (req, res) => {
-          res.json(teachers);
+      async function findTeacher() {
+        return new Promise((resolve, reject) => {
+          collteacher.find(
+            { subject: subject },
+            function (err, docs) {
+              if (err) {
+                console.log(err);
+                reject(err);
+              }
+              Info = docs;
+              resolve({ Info });
+            }
+          );
         });
+      }
+      const result = await findTeacher();
+
+      const teachers = [];
+      for (let i = 0; i < result.Info.length; i++) {
+        teachers.push({
+          value: result.Info[i].email,
+          text: result.Info[i].email,
+        });
+      }
+
+      app.get("/teachers", (req, res) => {
+        res.json(teachers);
       });
+
       res.sendFile(dic + "/HTML/create1.html");
     });
     //مانو مهم
@@ -108,7 +73,7 @@ module.exports = {
       };
       res.json(obj1);
     });
-    //fill out the selecte for coures
+    //checking the number of students in each course
     const options = [];
     for (let i = 0; i < selected_subject.length; i++) {
       async function dd() {
@@ -127,13 +92,8 @@ module.exports = {
         });
       }
       const result = await dd();
-      // console.log(result.Info);
-
-      console.log("hiiiiiiiiii");
       cnt = result.Info;
       subjCnt.push([cnt, selected_subject[i]]);
-      console.log(cnt + "mmmmmm");
-      console.log(subjCnt.length + "hhhhhhhhh");
     }
     /////////////////
     for (let i = 0; i < selected_level.length; i++) {
@@ -159,26 +119,16 @@ module.exports = {
       subjCnt.push([cnt, selected_level[i]]);
     }
 
-    ///////////////////////
+    //fill out the selecte for coures
     for (let j = 0; j < subjCnt.length; j++) {
       if (parseInt(subjCnt[j][0]) >= 1) {
         options.push({ value: subjCnt[j][1], text: subjCnt[j][1] });
-        console.log("hi");
       }
       console.log(subjCnt[j][1]);
     }
     app.get("/options", (req, res) => {
       res.json(options);
     });
-    // for (let i = 0; i < selected_subject.length; i++) {
-    // cnt = collection
-    //   .count({ selected_subject: selected_subject[i] })
-    //   .then((count) => {
-    //     // console.log(`Number of documents with selected_subject : ${count}`);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
   },
-  // },
+
 };
