28630aaccefa07fc240291474fc1b72e37bb0fcd
HananRstom <hananalrstom87@gamil.com>
2023-10-09 14:53:34 +0300

    showBook&editEmail

--- employee/js/book.js
+++ employee/js/book.js
@@ -14,6 +14,7 @@ module.exports = {
     let obj;
     let error;
     let book;
+    let showprice;
     app.post("/editBook", async function (req, res) {
       error = false;
       id = parseInt(req.body.bookId);
@@ -32,14 +33,54 @@ module.exports = {
         }
         const result = await nn();
         data = result.Info;
-
       }
-      if ((id == "") || data.length == 0)
-        error = true;
+      if (id == "" || data.length == 0) error = true;
       res.send();
     });
-    app.post("/delConf", function (req, res) {
+    let Book, price, totalPrice;
+    app.post("/price", function (req, res) {
       book = req.body.book;
+
+      collBook.findOne({ book_id: id }).then((document) => {
+        if (document && document.books && Array.isArray(document.books)) {
+          Book = document.books;
+          price = document.price;
+          totalPrice = document.totalPrice;
+
+          for (let i = 0; i < book.length; i++) {
+            for (let j = 0; j < Book.length; j++) {
+              let Index = -1;
+              if (book[i] === Book[j]) {
+                Index = j;
+                console.log("Index:", Index);
+                if (Index >= 0) {
+                  Book.splice(Index, 1);
+                  totalPrice -= price[Index];
+                  price.splice(Index, 1);
+                } else {
+                  console.log("The index is invalid");
+                }
+                break;
+              }
+            }
+          }
+        } else {
+          console.log("Document not found or invalid busy array");
+        }
+      });
+
+      res.send();
+    });
+    app.get("/showPrice", function (req, res) {
+      showprice = {
+        totalPrice: totalPrice,
+        Book: Book,
+        price: price,
+      };
+      res.json(showprice);
+    });
+    app.post("/delConf", function (req, res) {
+    
 
       collBook
         .findOne({ book_id: id })
@@ -58,7 +99,7 @@ module.exports = {
                   if (Index >= 0) {
                     Book.splice(Index, 1);
                     totalPrice -= price[Index];
-                    price.splice(Index, 1)
+                    price.splice(Index, 1);
                   } else {
                     console.log("The index is invalid");
                   }
@@ -67,15 +108,13 @@ module.exports = {
               }
             }
             if (Book.length == 0) {
-              return collBook.remove({ book_id: id })
-            }
-            else {
+              return collBook.remove({ book_id: id });
+            } else {
               return collBook.update(
                 { book_id: id },
                 { $set: { books: Book, price: price, totalPrice: totalPrice } }
               );
             }
-
           } else {
             console.log("Document not found or invalid busy array");
           }
@@ -86,13 +125,12 @@ module.exports = {
         .catch((error) => {
           console.error("Error while deleting row:", error);
         });
-      res.send()
-    })
+      res.send();
+    });
     app.get("/showBook", function (req, res) {
-
       obj = {
         result: data,
-        error: error
+        error: error,
       };
       res.json(obj);
     });
--- employee/js/editTeacher.js
+++ employee/js/editTeacher.js
@@ -8,25 +8,32 @@ module.exports = {
     const url = "mongodb://0.0.0.0:27017/languages";
     const db = monk(url);
     const collection = db.get("teachers");
-    var emp = false
+    var emp = false;
     var Fname1;
     var Mname1;
     var Lname1;
     var subject1;
     var wage1;
     var Email1 = "";
+    var email;
     var id;
     var obj1;
     var Wrong = false;
-
+    let check = " ";
+    var testEmail;
+    let accept_email = ["hotmail", "gmail"];
     app.post("/teacher-inf", (req, res) => {
-      id = ''
-      id = req.body.ID
+      id = "";
+      id = req.body.ID;
 
       res.send();
     });
     app.post("/teachEdit", (req, res) => {
-     subject1="", Fname1="", Lname1="", wage1="", Email1 = ""
+      (subject1 = ""),
+        (Fname1 = ""),
+        (Lname1 = ""),
+        (wage1 = ""),
+        (Email1 = "");
       Fname1 = req.body.f_name;
       Mname1 = req.body.m_name;
       Lname1 = req.body.l_name;
@@ -36,80 +43,108 @@ module.exports = {
 
       wage1 = req.body.wage;
       Email1 = req.body.Email;
+      email = Email1.toLowerCase();
+      if (Email1 != "") {
+        let isValidEmail = false;
+        for (let i = 0; i < accept_email.length; i++) {
+          if (email.includes("@" + accept_email[i] + ".com")) {
+            isValidEmail = true;
+            break;
+          }
+        }
+      
+        if (!isValidEmail) {
+          check = "Please check the email address entered and try again";
+        }
+      }
       obj1 = {
         Fname1: Fname1,
         Mname1: Mname1,
         Lname1: Lname1,
         subject1: subject1,
         wage1: wage1,
-        Email1: Email1
-      }
-
-      console.log(subject1)
+        Email1: Email1,
+      };
+      console.log(check);
+      console.log(subject1);
       res.send();
     });
     app.post("/confirmE", (req, res) => {
       if (Fname1 != "") {
-        collection.update({ ID: parseInt(id) }, { $set: { Fname: Fname1 } })
+        collection.update({ ID: parseInt(id) }, { $set: { Fname: Fname1 } });
       }
       if (Lname1 != "") {
-        collection.update({ ID: parseInt(id) }, { $set: { Lname: Lname1 } })
+        collection.update({ ID: parseInt(id) }, { $set: { Lname: Lname1 } });
       }
       if (Mname1 != "") {
-        collection.update({ ID: parseInt(id) }, { $set: { Mname: Mname1 } })
+        collection.update({ ID: parseInt(id) }, { $set: { Mname: Mname1 } });
       }
       if (Email1 != "") {
-        collection.update({ ID: parseInt(id) }, { $set: { email: Email1 } })
+        collection.update({ ID: parseInt(id) }, { $set: { email: Email1 } });
       }
       if (wage1 != "") {
-        collection.update({ ID: parseInt(id) }, { $set: { wage: wage1 } })
+        collection.update({ ID: parseInt(id) }, { $set: { wage: wage1 } });
       }
       if (subject1 != "") {
-        collection.update({ ID: parseInt(id) }, { $set: { subject: subject1 } })
+        collection.update(
+          { ID: parseInt(id) },
+          { $set: { subject: subject1 } }
+        );
       }
-      res.send("")
-    })
+      res.send("");
+    });
     app.get("/linkE", async function (req, res) {
-
       async function dd() {
         return new Promise((resolve, reject) => {
-          collection.find(
-            { ID: parseInt(id) },
-            function (err, docs) {
-              if (err) {
-                console.log(err);
-                reject(err);
-              }
-              Info = docs;
-              // console.log(Info);
-              resolve({ Info, Wrong });
-            })
-        })
-
+          collection.find({ ID: parseInt(id) }, function (err, docs) {
+            if (err) {
+              console.log(err);
+              reject(err);
+            }
+            Info = docs;
+            // console.log(Info);
+            resolve({ Info, Wrong });
+          });
+        });
       }
       const result = await dd();
 
       if (!isEmpty(result.Info)) {
-        console.log("hi")
+        console.log("hi");
         console.log(result.Info);
-      }
-      else {
+      } else {
         emp = true;
+      }
+
+      testEmail = false;
+
+      async function findTeacher() {
+        return new Promise((resolve, reject) => {
+          collection.find({ email: email }, function (err, docs) {
+            if (err) {
+              console.log(err);
+              reject(err);
+            }
+            Info = docs;
+            resolve({ Info });
+          });
+        });
+      }
+
+      const result2 = await findTeacher();
+      var result4 = result2.Info;
+      if (!isEmpty(result4)) {
+        testEmail = true;
       }
 
       let object = {
         result: result,
         obj1: obj1,
-        emp: emp
-
+        emp: emp,
+        check: check,
+        testEmail: testEmail,
       };
       res.json(object);
     });
-
-
-
-
-
-
-  }
-}
+  },
+};
